a944d9044131dbcb35de617272734d4749c210c1
sujal vaghela <1suujalvaghela1@gmail.com>
2025-02-09 01:30:51 +0530

    completed 1st

--- 01Three/index.js
+++ 01Three/index.js
@@ -3,7 +3,7 @@ const canvas = document.querySelector(".first");
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(2, 2, 2);
-const material = new THREE.MeshBasicMaterial({ color: "red" });
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
 const box = new THREE.Mesh(geometry, material);
 scene.add(box);
@@ -14,13 +14,21 @@ const size = {
 };
 
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
-camera.position.x = 2;
-// camera.position.y = 2;
-camera.position.z = 4;
+// camera.position.x = 0;
+// camera.position.y = 1;
+camera.position.z = 5;
 
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({ canvas });
 
 renderer.setSize(size.width, size.height);
-renderer.render(scene, camera);
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  box.rotation.x += 0.01;
+  box.rotation.y += 0.01;
+}
+
+animate();
